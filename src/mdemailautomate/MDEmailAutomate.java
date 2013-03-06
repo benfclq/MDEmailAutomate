9b503357468fa295e329153587161f65274a45a7
Ben Myers <benfclq@inbox.com>
2013-03-06 17:45:44 +0000

    minor updates

--- src/mdemailautomate/MDEmailAutomate.java
+++ src/mdemailautomate/MDEmailAutomate.java
@@ -18,7 +18,7 @@ public class MDEmailAutomate extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
         
         Scene scene = new Scene(root);
         
